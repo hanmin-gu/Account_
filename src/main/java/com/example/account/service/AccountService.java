c5fad7145be83228b9219810019e29e2c8cc6a83
mingu <mingu9968@gmail.com>
2023-05-25 10:44:54 +0900

    feat:createAccount

--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -5,12 +5,14 @@ import com.example.account.domain.Account;
 import com.example.account.domain.AccountUser;
 import com.example.account.repository.AccountRepository;
 import com.example.account.repository.AccountUserRepository;
+import com.example.account.type.AccountStatus;
 import com.example.account.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.AccountException;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -19,18 +21,27 @@ public class AccountService {
     private final AccountUserRepository accountUserRepository;
 
     @Transactional
-    public void createAccount(Long userId, Long initialBalance) {
+    public Account createAccount(Long userId, Long initialBalance) {
         AccountUser accountUser =
                 accountUserRepository.findById(userId).orElseThrow(() -> new accountException(ErrorCode.USER_NOT_FOUND));
 
         String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
                 .map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + "")
-                .orElse("10000000000");
+                .orElse("1000000000");
+
+        return accountRepository.save(
+                Account.builder()
+                        .accountUser(accountUser)
+                        .accountStatus(AccountStatus.IN_USE)
+                        .accountNumber(newAccountNumber)
+                        .balance(initialBalance)
+                        .registeredAt(LocalDateTime.now())
+                        .build());
     }
 
     @Transactional
     public Account getAccount(Long id) {
-        if(id < 0){
+        if (id < 0) {
             throw new RuntimeException("Minus");
         }
         return accountRepository.findById(id).get();
