39279b85bb0186238cba020afc20ecf6842d270b
mingu <mingu9968@gmail.com>
2023-05-27 12:45:46 +0900

    feat:accountDto entity생성 기능 작성

--- src/main/java/com/example/account/dto/AccountDto.java
+++ src/main/java/com/example/account/dto/AccountDto.java
@@ -17,8 +17,14 @@ public class AccountDto {
     private LocalDateTime registeredAt;
     private LocalDateTime unRegisteredAt;
 
+
     public static AccountDto fromEntity(Account account){
-        return AccountDto.builder().userId(account.getAccountUser().getId()).build();
+        return AccountDto.builder()
+                .userId(account.getAccountUser().getId())
+                .accountNumber(account.getAccountNumber())
+                .registeredAt(account.getRegisteredAt())
+                .unRegisteredAt(account.getUnRegisteredAt())
+                .build();
 
     }
 }
