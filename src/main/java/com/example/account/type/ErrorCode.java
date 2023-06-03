aefcfcc6df18a60ba79b18f47977a6e68f632b89
mingu <mingu9968@gmail.com>
2023-06-03 23:04:10 +0900

    feat:

--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND("사용자가 없습니다.");
+    USER_NOT_FOUND("사용자가 없습니다."),
+    ACCOUNT_NOT_FOUND("계좌가 없습니다."),
+    MAX_ACCOUNT_PER_USER_10("사용자 최대 계좌는 10개입니다.");
     private final String description;
 }
